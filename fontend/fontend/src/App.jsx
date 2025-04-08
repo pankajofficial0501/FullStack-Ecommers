259390b26c441c7e7500a4fd16ff90ca42eab88a
pankaj0501 <Pankajofficial0501@gmail.com>
2025-04-08 23:59:41 +0530

    Featured Section, ProductGrid and ProductDetails

--- fontend/fontend/src/App.jsx
+++ fontend/fontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes  } from "react-router-dom"
 import UserLayout from "./components/Layout/UserLayout"
 import Home from "./Pages/Home"
+import { Toaster } from "sonner"
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Toaster position = "top-right" />
     <Routes>
       <Route path="/" element={<UserLayout />}>
         <Route index element = {<Home />} />
---
+++ fontend/fontend/src/components/Products/FeatureSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FeatureSection = () => {
+  return (
+    <section className='py-16 px-4 bg-white'>
+      <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 text-center gap-8'>
+        <div className='flex flex-col items-center'>
+            <div className='p-4 rounded-full mb-4'>
+                
+            </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default FeatureSection
